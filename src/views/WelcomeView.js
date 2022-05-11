659daae47049ead4a5467d77ef6af4f59a4849cf
artryan130 <80287857+artryan130@users.noreply.github.com>
2022-05-11 20:30:55 -0300

    Estilização WelcomeView

--- src/views/WelcomeView.js
+++ src/views/WelcomeView.js
@@ -4,12 +4,14 @@ import {Text, View, StyleSheet, Image, Button } from "react-native";
 export default props => {
     return (
         <View style={style.welcome}>
-            <Text>Bem vindo ao iResenha</Text>
+            <Text style={style.title}>Bem vindo ao iResenha</Text>
             <Image
+            style={style.image}
             source={require('../../assets/image7.png')}
             />
-            <Text>O melhor lugar para encontrar e gerenciar atividades !</Text>
-            <Button 
+            <Text style={style.text}>O melhor lugar para encontrar e gerenciar atividades !</Text>
+            <Button
+            style={style.button} 
             title="Iniciar"
             />
         </View>
@@ -18,6 +20,34 @@ export default props => {
 }
 
 const style = StyleSheet.create({
+    welcome: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 10,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textDecorationLine: "underline",
+        textDecorationColor: '#23C7D7',
+        marginTop: 30
+    },
+    image: {
+        marginTop: 100,
+        marginBottom: 50,
+    },
+    text: {
+        marginBottom: 30,
+        fontSize: 20,
+        textAlign: "center"
+    },
+    button: {
+    }
+
+
+
 
 
 
