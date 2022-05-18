9ee4a8c3c9210a57d45ba4d4a29faef46d4ed16c
artryan130 <80287857+artryan130@users.noreply.github.com>
2022-05-18 19:44:33 -0300

    Finalizando pág NameView

--- src/views/NameView.js
+++ src/views/NameView.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Text, View, StyleSheet, TextInput } from "react-native"; 
+import {Text, View, StyleSheet, TextInput, Image } from "react-native"; 
 import { Button } from 'react-native-elements';
 
 export function NameView() {
@@ -7,14 +7,19 @@ export function NameView() {
     const [user, setUser] = useState('')
 
     return (
-        <View>
-            <Text>
-                Olá! Insira o seu nome para continuarmos.
+        <View style={style.name}>
+            <Text style={style.text}>
+                Olá! Insira o seu nome para continuarmos
             </Text>
             <TextInput
                 onChangeText={name => setUser({...user, name })} 
                 placeholder="Digite o seu nome"
                 value={user.name}
+                style={style.input}
+            />
+            <Image
+            style={style.image}
+            source={require('../../assets/image8.png')}
             />
             <Button
                 buttonStyle={{
@@ -24,7 +29,43 @@ export function NameView() {
                     backgroundColor: '#23C7D7',
                 }}
                 title="Continuar"
+                style={style.button}
             />
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    name: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 10,
+    },
+    text: {
+        fontSize: 30,
+        // fontFamily: 'Poppins-Medium',
+        fontWeight: 'bold',
+        marginTop: 30,
+        textAlign: 'center'
+    },
+    input: {
+        width: 300,
+        height: 50,
+        marginTop: 200,
+        marginBottom: 140,
+        borderWidth: 1,
+        borderColor: '#23C7D7',
+        borderRadius: 10,
+        paddingLeft: 40
+    },
+    image: {
+        position: "absolute",
+        marginTop: 340,
+        left: 70,
+    }
+
+
+
+})
